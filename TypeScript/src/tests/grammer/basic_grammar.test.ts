bd09cf3df183f7d7354867bbd04a28eea3b03356
Kyohei Ozawa <zawazawakyo@gmail.com>
2024-10-21 21:54:30 +0900

    feat: Add tests for || and ?? operators

---
+++ TypeScript/src/tests/grammer/basic_grammar.test.ts
@@ -0,0 +1,65 @@
+describe("||演算子と??演算子の違い", () => {
+  const default_val = "default";
+  const null_val = null;
+  const undefined_val = undefined;
+
+  describe("||演算子", () => {
+    test("nullやundefinedに対してデフォルト値を戻す", () => {
+      expect(
+        null_val || default_val
+      ).toBe(default_val);
+
+      expect(
+        undefined_val || default_val
+      ).toBe(default_val);
+    });
+
+    test("は空文字や0, falseでもデフォルト値を戻す", () => {
+      const zero = 0;
+      const empty_str = "";
+      const false_val = false;
+
+      expect(
+        zero || default_val
+      ).toBe(default_val);
+
+      expect(
+        empty_str || default_val
+      ).toBe(default_val);
+
+      expect(
+        false_val || default_val
+      ).toBe(default_val);
+    });
+  })
+
+  describe("??演算子", () => {
+    test("nullやundefinedに対してデフォルト値を戻す (||演算子と同じ)", () => {
+      expect(
+        null_val ?? default_val
+      ).toBe(default_val);
+
+      expect(
+        undefined_val ?? default_val
+      ).toBe(default_val);
+    });
+
+    test("は空文字や0, falseでもデフォルト値を戻す (||演算子との違い)", () => {
+      const zero = 0;
+      const empty_str = "";
+      const false_val = false;
+
+      expect(
+        zero ?? default_val
+      ).toBe(zero);
+
+      expect(
+        empty_str ?? default_val
+      ).toBe(empty_str);
+
+      expect(
+        false_val ?? default_val
+      ).toBe(false_val);
+    });
+  })
+});
