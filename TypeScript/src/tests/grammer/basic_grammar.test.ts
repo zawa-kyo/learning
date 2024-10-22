a1c582c10e5bbfe3683e1008fb08fc08856cd481
Kyohei Ozawa <zawazawakyo@gmail.com>
2024-10-22 22:39:52 +0900

    feat: Add tests for Number type

--- TypeScript/src/tests/grammer/basic_grammar.test.ts
+++ TypeScript/src/tests/grammer/basic_grammar.test.ts
@@ -1,3 +1,29 @@
+describe("NumberはIEEE 754倍浮動小数点数", () => {
+  test("0.1 + 0.2 = 0.3にはならない", () => {
+    expect(0.1 + 0.2).not.toBe(0.3);
+    expect(0.1 + 0.2).toBe(0.30000000000000004);
+
+    // info: JestではtoBeCloseToなら桁数で区切って判別できる
+    expect(0.1 + 0.2).toBeCloseTo(0.3);
+  });
+
+  describe("仮数部の精度は53ビット", () => {
+    const ceilingMinusOne = 9007199254740991; // 2^53 - 1
+    const ceiling = 9007199254740992; // 2^53
+    const ceilingPlusOne = 9007199254740993; //2^53 + 1
+
+    test("2^53までは表せるが、", () => {
+      expect(ceilingMinusOne.toString()).toBe("9007199254740991");
+      expect(ceiling.toString()).toBe("9007199254740992");
+    });
+
+    test("2^53+1では誤差が生じる", () => {
+      expect(ceilingPlusOne.toString()).not.toBe("9007199254740993");
+      expect(ceilingPlusOne.toString()).toBe("9007199254740992");
+    });
+  });
+});
+
 describe("||演算子と??演算子の違い", () => {
   const default_val = "default";
   const null_val = null;
