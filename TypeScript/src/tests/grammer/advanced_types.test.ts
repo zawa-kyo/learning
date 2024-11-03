8e80a62eab935dee10b00f7fcd588ff8bfe3097e
Kyohei Ozawa <zawazawakyo@gmail.com>
2024-11-03 18:54:50 +0900

    feat: Add combining keyof type, lookup type, and generics test

--- TypeScript/src/tests/grammer/advanced_types.test.ts
+++ TypeScript/src/tests/grammer/advanced_types.test.ts
@@ -67,3 +67,127 @@ describe("typeof型", () => {
     });
   });
 });
+
+describe("keyof型、lookup型とジェネリクスを組み合わせる", () => {
+  test("ジェネリクスを組み合わせて型推論する", () => {
+    // T[K]はlookup型なのでget<T, K>(...)だとコンパイルエラー
+    function get<T, K extends keyof T>(obj: T, key: K): T[K] {
+      return obj[key];
+    }
+
+    type Human = {
+      name: string;
+      age: number;
+    };
+
+    const taro: Human = {
+      name: "taro",
+      age: 25,
+    };
+
+    // 型推論はget<Human, "name">(obj: Human, key: "name"): string
+    // 第2引数は "name" | "age" の部分型しか受け入れない
+    expect(get(taro, "name")).toBe("taro");
+  });
+});
+
+describe("as const", () => {
+  test("配列リテラルの型推論結果を配列型ではなくタプル型にする", () => {
+    // readonlyかでタプルか判定する
+    type IsTuple<T> = T extends readonly [infer A, ...infer B] ? true : false;
+
+    // 通常の配列リテラル
+    const names = ["taro", "john", "heinz"];
+    type IsNamesTuple = IsTuple<typeof names>;
+    const isNamesTuple: IsNamesTuple = false; // "false"型なので代入できる
+    expect(isNamesTuple).toBe(false);
+
+    // タプルとして扱う配列リテラル (readonly)
+    const names_readonly = ["taro", "john", "heinz"] as const;
+    type IsNamesReadonlyTuple = IsTuple<typeof names_readonly>;
+    const isNamesReadonlyTuple: IsNamesReadonlyTuple = true; // "true"型なので代入できる
+    expect(isNamesReadonlyTuple).toBe(true);
+  });
+
+  test("値から型をつくる", () => {
+    const names = ["taro", "john", "heinz"] as const;
+
+    // lookup型 (T[K]) から型情報を取り出せる
+    type Name = (typeof names)[number];
+
+    // Nameは"taro" | "john" | "heinz"なので"taro"は代入できる
+    const name: Name = "taro";
+
+    expect(name).toBe("taro");
+  });
+});
+
+describe("型ガード", () => {
+  test("ユーザー定義型型ガードで型を保証する", () => {
+    function isStringOrNumber(value: unknown): value is string | number {
+      return typeof value === "string" || typeof value === "number";
+    }
+
+    const stringValue: unknown = "hello";
+    const booleanValue: unknown = true;
+
+    if (isStringOrNumber(stringValue)) {
+      const result: string | number = stringValue;
+      expect(typeof result === "string" || typeof result === "number").toBe(
+        true
+      );
+    }
+
+    if (!isStringOrNumber(booleanValue)) {
+      const result: boolean = booleanValue as boolean;
+      expect(typeof result === "boolean").toBe(true);
+    }
+  });
+
+  test("asserts型ガードは関数が無事終了すれば引数はその型であることを保証する", () => {
+    type Human = {
+      name: string;
+      age: number;
+    };
+
+    function assertHuman(value: any): asserts value is Human {
+      if (value == null) {
+        throw new Error("Given value is null or undefined!");
+      }
+
+      if (
+        value.type !== "Human" ||
+        typeof value.name !== "string" ||
+        typeof value.age !== "number"
+      ) {
+        throw new Error("Given value is not a Human!");
+      }
+    }
+
+    // 正しいHumanオブジェクトが渡った場合、例外をスローしない
+    const validHuman = { type: "Human", name: "John", age: 30 };
+    expect(() => assertHuman(validHuman)).not.toThrow();
+
+    // 不正なオブジェクトなら例外をスローする
+    const invalidHumanType = { type: "Animal", name: "John", age: 30 };
+    expect(() => assertHuman(invalidHumanType)).toThrow(
+      "Given value is not a Human!"
+    );
+
+    // 型が異なる場合、例外をスローする
+    const invalidHumanName = { type: "Human", name: 123, age: 30 };
+    expect(() => assertHuman(invalidHumanName)).toThrow(
+      "Given value is not a Human!"
+    );
+
+    const invalidHumanAge = { type: "Human", name: "John", age: "30" };
+    expect(() => assertHuman(invalidHumanAge)).toThrow(
+      "Given value is not a Human!"
+    );
+
+    // assertHuman通過後はHuman型であることが保証され、プロパティにアクセスできる
+    assertHuman(validHuman);
+    expect(validHuman.name).toBe("John");
+    expect(validHuman.age).toBe(30);
+  });
+});
