6b896bd285d7df625581888f9cd61bab18d6f456
Kyohei Ozawa <zawazawakyo@gmail.com>
2024-11-29 22:59:20 +0900

    fix: Fix issue where theme resets after screen transition

--- Flutter/apps/hook_routing/lib/main.dart
+++ Flutter/apps/hook_routing/lib/main.dart
@@ -5,40 +5,38 @@ import 'package:hook_routing/pages/countUp.dart';
 import 'package:hook_routing/pages/top.dart';
 
 void main() {
-  runApp(MaterialApp.router(
-    routerConfig: _router,
-  ));
+  runApp(const MyApp());
 }
-
-final _router = GoRouter(routes: [
-  GoRoute(
-    path: '/',
-    builder: (context, state) => const MyApp(),
-    routes: [
-      GoRoute(
-        path: '/countUp',
-        builder: (context, state) => const CountUp(),
-      ),
-      GoRoute(
-        path: '/countDown',
-        builder: (context, state) => const CountDown(),
-      ),
-    ],
-  ),
-]);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    final _router = GoRouter(routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const Top(),
+        routes: [
+          GoRoute(
+            path: '/countUp',
+            builder: (context, state) => const CountUp(),
+          ),
+          GoRoute(
+            path: '/countDown',
+            builder: (context, state) => const CountDown(),
+          ),
+        ],
+      ),
+    ]);
+
+    return MaterialApp.router(
+      routerConfig: _router,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: const Top(),
     );
   }
 }
